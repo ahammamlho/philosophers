7d102edd845379a8875132e75538bc4c8e0ab720
AHAMMAM LHOUSSAINE <ahammamlho96@gmail.com>
2022-06-24 12:14:26 +0100

    ff

--- philo_bonus/philo_bonus.h
+++ philo_bonus/philo_bonus.h
@@ -6,7 +6,7 @@
 /*   By: ahammam <ahammam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 22:23:18 by ahammam           #+#    #+#             */
-/*   Updated: 2022/06/22 22:24:14 by ahammam          ###   ########.fr       */
+/*   Updated: 2022/06/24 12:13:41 by ahammam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,11 @@
 #include <sys/time.h>
 #include <stdbool.h>
 #include <stdio.h>
+
+#define TAKEN_FORK 1
+#define EATING 2
+#define SLEEPING 3
+#define THINKING 4
+#define DEAD 5
 
 #endif
